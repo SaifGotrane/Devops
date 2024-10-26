68e77de52a97deed496516f5b482df14a7b30898
SaifGotrane <saifeddine.gotrane@esprit.tn>
2024-10-26 16:24:35 +0100

    update pom.xml

---
+++ src/test/java/tn/esprit/devops_project/ExempleTest.java
@@ -0,0 +1,12 @@
+package tn.esprit.devops_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExempleTest {
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
